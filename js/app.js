292d7ae97d4021b1221aec0e75ed37302b28942e
Ezequiel Faustino <ezequielfaustino@gmail.com>
2023-07-16 19:47:57 -0400

    metodo filter p/ ver quantos players Real comprou

--- js/app.js
+++ js/app.js
@@ -1,27 +1,45 @@
 const topTransfers = [
-  {player: 'Arthur', from: 'Grêmio', to: 'Barcelona', amount: 40_000000},
-  {player: 'Lucas Moura', from: 'São Paulo', to: 'PSG', amount: 43_000000},
-  {player: 'Rodrygo', from: 'Santos', to: 'Real Madrid', amount: 45_000000},
-  {player: 'Vinícius Júnior', from: 'Flamengo', to: 'Real Madrid', amount: 45_000000},
-  {player: 'Endrick', from: 'Palmeiras', to: 'Real Madrid', amount: 72_000000},
-  {player: 'Vitor Roque', from: 'Athletico-PR', to: 'Barcelona', amount: 74_000000},
-  {player: 'Neymar', from: 'Santos', to: 'Barcelona', amount: 88_400000},
+  {player: 'Arthur', from: 'Grêmio', to: 'Barcelona', amount: 40},
+  {player: 'Lucas Moura', from: 'São Paulo', to: 'PSG', amount: 43},
+  {player: 'Rodrygo', from: 'Santos', to: 'Real Madrid', amount: 45},
+  {player: 'Vinícius Júnior', from: 'Flamengo', to: 'Real Madrid', amount: 45},
+  {player: 'Endrick', from: 'Palmeiras', to: 'Real Madrid', amount: 72},
+  {player: 'Vitor Roque', from: 'Athletico-PR', to: 'Barcelona', amount: 74},
+  {player: 'Neymar', from: 'Santos', to: 'Barcelona', amount: 88},
 ]
+
+const realBought = topTransfers.filter(({to}) => to === 'Real Madrid' )
+const barcaBought = topTransfers.filter(({to}) => to === 'Barcelona')
+
 
 const renderTransfers = () => {
   const transfersList = document.querySelector('[data-js="transfers-list"]')
   const topBuyers = document.querySelector('[data-js="top-buyers"]')
   const buyersFragment = document.createDocumentFragment()
-  
-  const realBought = topTransfers.filter(({to}) => to === 'Real Madrid' )
+  const transfersFragment = document.createDocumentFragment()
+
+  topTransfers.forEach(({player, from, to, amount}) => {
+    const li = document.createElement('li')
+    li.textContent = `${player}; do ${from}; para o ${to}; €${amount} milhões.`
+    
+    transfersFragment.append(li)
+  })
+
   realBought.forEach(({player, from, to}) => {
     const li = document.createElement('li')
     li.textContent = `${player}; do ${from}; para o ${to}.`
     buyersFragment.append(li)
   })
 
-  topBuyers.append(buyersFragment)
+  barcaBought.forEach(({player, from, to}) => {
+    const li = document.createElement('li')
+    li.textContent = `${player}; do ${from}; para o ${to}.`
+    
+    buyersFragment.append(li)
+  })
 
+  topBuyers.append(buyersFragment)
+  transfersList.append(transfersFragment)
   
 
 }
