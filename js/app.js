458a98d5e13fb7d58cb3d9abcda575585f635246
Ezequiel Faustino <ezequielfaustino@gmail.com>
2023-07-17 15:39:36 -0400

    usado reduce para somar o valor pago nas transfers

--- js/app.js
+++ js/app.js
@@ -1,18 +1,15 @@
 const topTransfers = [
-  {player: 'Arthur', from: 'Grêmio', to: 'Barcelona', amount: 40},
-  {player: 'Lucas Moura', from: 'São Paulo', to: 'PSG', amount: 43},
-  {player: 'Rodrygo', from: 'Santos', to: 'Real Madrid', amount: 45},
-  {player: 'Vinícius Júnior', from: 'Flamengo', to: 'Real Madrid', amount: 45},
-  {player: 'Endrick', from: 'Palmeiras', to: 'Real Madrid', amount: 72},
-  {player: 'Vitor Roque', from: 'Athletico-PR', to: 'Barcelona', amount: 74},
-  {player: 'Neymar', from: 'Santos', to: 'Barcelona', amount: 88},
+  { player: 'Arthur', from: 'Grêmio', to: 'Barcelona', amount: 40 },
+  { player: 'Lucas Moura', from: 'São Paulo', to: 'PSG', amount: 43 },
+  { player: 'Rodrygo', from: 'Santos', to: 'Real Madrid', amount: 45 },
+  { player: 'Vinícius Júnior', from: 'Flamengo', to: 'Real Madrid', amount: 45 },
+  { player: 'Endrick', from: 'Palmeiras', to: 'Real Madrid', amount: 72 },
+  { player: 'Vitor Roque', from: 'Athletico-PR', to: 'Barcelona', amount: 74 },
+  { player: 'Neymar', from: 'Santos', to: 'Barcelona', amount: 88 },
 ]
 
-const realBought = topTransfers.filter(({to}) => to === 'Real Madrid' )
-const barcaBought = topTransfers.filter(({to}) => to === 'Barcelona')
-
-const realSpent = realBought.reduce((accumulator, {amount}) => accumulator + amount, 0)
-console.log(realSpent);
+const realBought = topTransfers.filter(({ to }) => to === 'Real Madrid')
+const barcaBought = topTransfers.filter(({ to }) => to === 'Barcelona')
 
 const renderTransfers = () => {
   const transfersList = document.querySelector('[data-js="transfers-list"]')
@@ -20,30 +17,51 @@ const renderTransfers = () => {
   const buyersFragment = document.createDocumentFragment()
   const transfersFragment = document.createDocumentFragment()
 
-  topTransfers.forEach(({player, from, to, amount}) => {
+  topTransfers.forEach(({ player, from, to, amount }) => {
     const li = document.createElement('li')
     li.textContent = `${player}; do ${from}; para o ${to}; €${amount} milhões.`
-    
+
     transfersFragment.append(li)
   })
 
-  realBought.forEach(({player, from, to}) => {
+  realBought.forEach(({ player, from, to }) => {
     const li = document.createElement('li')
     li.textContent = `${player}; do ${from}; para o ${to}.`
     buyersFragment.append(li)
   })
 
-  barcaBought.forEach(({player, from, to}) => {
+  barcaBought.forEach(({ player, from, to }) => {
     const li = document.createElement('li')
     li.textContent = `${player}; do ${from}; para o ${to}.`
-    
+
     buyersFragment.append(li)
   })
 
   topBuyers.append(buyersFragment)
   transfersList.append(transfersFragment)
-  
+}
 
+const realTransferInfo = () => {
+  const realSpent = realBought.reduce((accumulator, { amount }) => accumulator + amount, 0)
+  
+  const div = document.querySelector('[data-js="real-spent"]')
+  
+  const span = document.createElement('span')
+  span.innerText = `* Real Madrid gastou ${realSpent} milhoẽs de euros!`
+
+  div.append(span)
+}
+
+const barcaTransferInfo = () => {
+  const barcaSpent = barcaBought.reduce((accumulator, {amount}) => accumulator + amount, 0)
+  const div = document.querySelector('[data-js="barca-spent"]')
+  
+  const span = document.createElement('span')
+  span.innerText = `* Barcelona gastou ${barcaSpent} milhões de euros!`
+  
+  div.append(span)
 }
 
 renderTransfers()
+realTransferInfo()
+barcaTransferInfo()
